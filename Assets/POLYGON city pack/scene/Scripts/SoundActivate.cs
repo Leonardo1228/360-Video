7556508829e39c0e418e792f1e019f97b926a31f
Leonardo1228 <slvivas@uniboyaca.edu.co>
2026-03-06 15:55:13 -0500

    Added sounds

--- Assets/POLYGON city pack/scene/Scripts/SoundActivate.cs
+++ Assets/POLYGON city pack/scene/Scripts/SoundActivate.cs
@@ -8,17 +8,17 @@ public class Activarsonido : MonoBehaviour
     public GameObject post1;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "")
+        if (other.tag == "Act")
         {
-            AudioManager.Instance.Play3D("", post.transform.position);
+            AudioManager.Instance.Play3D("Sound", post.transform.position);
             //AudioManager.Instance.Play2D("");
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "")
+        if (other.tag == "Act")
         {
-            AudioManager.Instance.Play3D("", post1.transform.position);
+            AudioManager.Instance.Play3D("Wind", post1.transform.position);
             //AudioManager.Instance.Play2D("Salir");
         }
     }
